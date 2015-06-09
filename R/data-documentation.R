d04f4e025bb63cdb8fdf468826abcf126ed3b712
Francis DiTraglia <fditra@sas.upenn.edu>
2015-06-09 13:43:13 -0400

    add prelim documentation for factor and portfolio data

---
+++ R/data-documentation.R
@@ -0,0 +1,44 @@
+#' Fama/French 5 Factors (2x3)
+#'
+#' @description Monthly observations for the Fama & French (2015) five-factor
+#' model.
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/
+#' data_library.html}
+#' @references \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/
+#' Data_Library/f-f_5_factors_2x3.html}
+"ff5"
+
+#' Momentum Factor
+#'
+#' Monthly observations for the momentum factor.
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/
+#' data_library.html}
+#' @references \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/det_mom_factor.html}
+"mom"
+
+#' Liquidity Factor
+#'
+#' @description Monthly observations for the liquidity factor of Pastor &
+#' Stambaugh (2003).
+#' @source \url{http://faculty.chicagobooth.edu/lubos.pastor/research/}
+#' @references \url{http://faculty.chicagobooth.edu/lubos.pastor/research/
+#' liq_data_1962_2014.txt}
+"liq"
+
+#' Portfolios Formed on Size
+#'
+#' @description Monthly returns for portfolios formed by quantiles of size.
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/
+#' data_library.html}
+#' @references \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/
+#' Data_Library/det_port_form_sz.html}
+"size"
+
+#' 10 Industry Portfolios
+#'
+#' @description Monthly returns for ten portfolios formed by industry.
+#' @source \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/
+#' data_library.html}
+#' @references \url{http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/
+#' Data_Library/det_10_ind_port.html}
+"industry10"
