28d3ee1a6890d9011e579c5fcc90341b5975bbe4
Francis DiTraglia <fditra@sas.upenn.edu>
2015-06-09 19:08:17 -0400

    start documentation for zoofactr pacakge *itself*

---
+++ R/zoofactr.R
@@ -0,0 +1,31 @@
+#' zoofactr: A Bayesian visits the factor zoo.
+#'
+#' The zoofactr package contains functions and datasets for evaluating the
+#' importance of various factors thought to explain the cross-section of asset
+#' returns using Bayesian techniques.
+#'
+#' @section zoofactr main functions:
+#'
+#' @section zoofactr helper functions:
+#' \itemize{
+#'    \item \code{\link{density_normal}}
+#'    \item \code{\link{draw_normal}}
+#'    \item \code{\link{log_mv_gamma}}
+#'    \item \code{\link{density_wishart}}
+#'    \item \code{\link{draw_wishart}}
+#'    \item \code{\link{vech}}
+#'    \item \code{\link{devech}}
+#' }
+#'
+#' @section zoofactr datasets:
+#' \itemize{
+#'    \item \code{\link{ff5}}
+#'    \item \code{\link{industry10}}
+#'    \item \code{\link{liq}}
+#'    \item \code{\link{mom}}
+#'    \item \code{\link{size}}
+#' }
+#'
+#' @docType package
+#' @name zoofactr
+NULL
