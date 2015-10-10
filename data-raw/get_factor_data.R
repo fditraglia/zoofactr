9cc5cd9883f434114fd656655ad0c2d98119f8cd
irinapim <irina.v.pimenova@gmail.com>
2015-10-10 14:31:28 -0400

    5 more factors

--- data-raw/get_factor_data.R
+++ data-raw/get_factor_data.R
@@ -1,6 +1,7 @@
 #' @import xlsx
 #-----------------------------------------------------
 # Liquidity Factor (liq_data_1962_2014.txt)
+# LIQ
 # Source: Lubos Pastor's Website
 # (faculty.chicagobooth.edu/lubos.pastor/research/)
 #-----------------------------------------------------
@@ -10,6 +11,8 @@ devtools::use_data(liq)
 
 #-----------------------------------------------------
 # Momentum Factor (F-F_Momentum_Factor.zip)
+# MOM
+# Type: Returns
 # Source: Ken French's Website
 # (mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp)
 #-----------------------------------------------------
@@ -32,6 +35,8 @@ devtools::use_data(mom)
 
 #-----------------------------------------------------
 # Fama/French 5 Factors (2x3)
+# FF5
+# Type: Returns (unadjusted except for market)
 # Source: Ken French's Website
 # (mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp)
 #-----------------------------------------------------
@@ -53,6 +58,8 @@ devtools::use_data(ff5)
 
 #-----------------------------------------------------
 # Betting Against Beta
+# BAB
+# Type: Returns (unadjusted?)
 # Source: AQR website
 # (www.aqr.com/library/data-sets/betting-against-beta-equity-factors-monthly)
 #-----------------------------------------------------
@@ -81,6 +88,8 @@ rm(bab)
 
 #-----------------------------------------------------
 # Gross Profitability
+# GP
+# Type: Returns (unadjusted?)
 # Source: Robert Novy-Marx website
 # (http://rnm.simon.rochester.edu/data_lib/index.html)
 #-----------------------------------------------------
@@ -91,13 +100,15 @@ download.file(paste0(url_prefix, file_prefix, ".xlsx"), temp, mode = 'wb')
 gp <- read.xlsx(temp, 1, stringsAsFactors = FALSE)
 unlink(temp)
 gp <- gp[ , c("Date", "PMU.")]
-colnames(gp) <- c("month", "PMU")
+colnames(gp) <- c("month", "GP")
 rm(url_prefix, file_prefix, temp)
 devtools::use_data(gp)
 rm(gp)
 
 #-----------------------------------------------------
 # Quality Minus Junk
+# QMJ
+# Type: Returns (unadjusted?)
 # Source: AQR website
 # (https://www.aqr.com/library/data-sets/quality-minus-junk-factors-monthly)
 #-----------------------------------------------------
@@ -126,6 +137,8 @@ rm(qmj)
 
 #-----------------------------------------------------
 # Profitability and Investment
+# HXZ: ME - size, IA - investment, ROE - profitability
+# Type: Returns adjusted by the risk free rate
 # Source: provided by Lu Zhang
 # (zhanglu@fisher.osu.ed)
 #-----------------------------------------------------
@@ -135,6 +148,7 @@ hxz <- read.csv(paste0(file_dir, file_name), header = TRUE)
 hxz$Month <- paste0(hxz$Year, sprintf("%02d", hxz$Month))
 hxz$Year <- NULL
 colnames(hxz)[1] <- "month"
+colnames(hxz)[4] <- "IA"
 
 rm(file_dir, file_name)
 devtools::use_data(hxz)
@@ -142,14 +156,135 @@ rm(hxz)
 
 #-----------------------------------------------------
 # Common Idiosyncratic Volatility
+# CIV: CIV and CIVinnvov
+# Type: Returns (unadjusted?)
 # Source: provided by Brian Kelly
 # (bryan.kelly@chicagobooth.edu)
 #-----------------------------------------------------
 file_dir <- "data/"
 file_name <- "CIV_time_series.csv"
 civ <- read.csv(paste0(file_dir, file_name), header = TRUE)
-colnames(civ)[1] <- "month"
+colnames(civ) <- c("month", "CIV", "CIVinnov")
 
 rm(file_dir, file_name)
 devtools::use_data(civ)
 rm(civ)
+
+#-----------------------------------------------------
+# Default risk
+# DSV
+# Type: Macro indicator
+# Source: Maria Vassalou's site
+# http://maria-vassalou.com/research/data/
+#-----------------------------------------------------
+url <- "http://maria-vassalou.com/wp-content/uploads/2013/05/DSV.xls"
+temp <- tempfile()
+download.file(url, temp, mode = 'wb')
+dsv <- read.xlsx(temp, 1, stringsAsFactors = FALSE)
+unlink(temp)
+colnames(dsv) <- c("month", "DSV")
+
+rm(url, temp)
+devtools::use_data(dsv)
+rm(dsv)
+
+#-----------------------------------------------------
+# CAY(Consumption-wealth ratio) and Consumption Growth
+# CAY: CAY and dC
+# Type: Macro indicators
+# Source: Martin Lettau's site
+# http://faculty.haas.berkeley.edu/lettau/data/
+#-----------------------------------------------------
+cay <- readLines("http://faculty.haas.berkeley.edu/lettau/data/cay_q_03Q1.txt")
+monthly_obs <- grepl("[[:digit:]]{6}", substr(cay, 2, 7))
+first_line <- min(which(monthly_obs)) - 1
+last_line <- max(which(monthly_obs))
+cay <- cay[first_line:last_line]
+cay <- read.table(textConnection(cay), na.strings = c("-99.99", "-999"),
+                  header = TRUE, fill = TRUE)
+colnames(cay) <- c('month', 'c', 'a', 'y', 'cay')
+consGrowth = diff(cay[ , "c"])
+cay <- data.frame(month = cay$month[-1], DC = consGrowth, CAY = cay$cay[-1])
+mom <- data.frame(month = rownames(mom), mom, stringsAsFactors = FALSE)
+
+rm(monthly_obs, first_line, last_line)
+devtools::use_data(cay)
+rm(cay)
+
+#-----------------------------------------------------
+# Standartized Unexpected Earnings
+# SUE
+# Type: Returns
+# Source: Lakshmanan Shivakumar's website
+# https://www.london.edu/faculty-and-research/faculty/profiles/shivakumar-l
+#-----------------------------------------------------
+url <- "https://www.london.edu/-/media/files/faculty%20and%20research/faculty%20profiles/accounting/lakshmanan%20shivakumar/sue_portfolio_pmn_returns_nyseamex_2003.xls"
+temp <- tempfile()
+download.file(url, temp, mode = 'wb')
+sue <- read.xlsx(temp, 1, stringsAsFactors = FALSE)
+unlink(temp)
+colnames(sue) <- c("month", paste0("P", 1:10), "SUE")
+sue <- sue[, c("month", "SUE")]
+
+rm(url, temp)
+devtools::use_data(sue)
+rm(sue)
+
+#-----------------------------------------------------
+# Net Payout Yield
+# NPY: NPYAll and NPYNonFin
+# Type: Returns
+# Source: Michael Roberts' website
+# http://finance.wharton.upenn.edu/~mrrobert/styled-9/styled-13/index.html
+#-----------------------------------------------------
+url <- "http://finance.wharton.upenn.edu/~mrrobert/resources/Data/Post2Web-PayoutPaperDataTS-23-Sep-2011.xlsx"
+temp <- tempfile()
+download.file(url, temp, mode = 'wb')
+npy <- read.xlsx(temp, 2, stringsAsFactors = FALSE)
+unlink(temp)
+header_line <- which(npy[ , 1] == 'year')
+header <- npy[header_line, ]
+first_line <- header_line + 2
+last_line <- max(which(!is.na(npy[ , 1]) & npy[, 1] != ''))
+npy <- data.frame(npy[first_line : last_line, ])
+colnames(npy) <- header
+npy$month <- as.numeric(npy$month)
+npy$month <- as.numeric(npy$year) * 100 + as.numeric(npy$month)
+npy <- npy[which(npy[, 'lcrspnpy'] != '.'), ]
+npy <- npy[, c(2, which(header == 'lcrspnpy'))]
+colnames(npy) <- c('month', 'NPYAll', 'NPYNonFin')
+npy$NPYAll <- as.numeric(npy$NPYAll)
+npy$NPYNonFin <- as.numeric(npy$NPYNonFin)
+
+devtools::use_data(npy)
+rm(npy, header, header_line, first_line, last_line)
+
+#-----------------------------------------------------
+# Market Mispricing
+# UMO
+# Source: Dangling Jiang's website
+# https://sites.google.com/site/danlingjiang/data-library
+#-----------------------------------------------------
+key <- 'ZGVmYXVsdGRvbWFpbnxkYW5saW5namlhbmd8Z3g6NjU4ZTMzOGVmMDk4ZTE0Mw'
+key <- "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxkYW5saW5namlhbmd8Z3g6NjU4ZTMzOGVmMDk4ZTE0Mw"
+temp <- tempfile()
+download.file(url, temp, mode = 'wb', method = "libcurl")
+umo <- read.xlsx(temp, 2, stringsAsFactors = FALSE)
+unlink(temp)npy <- read.xlsx(temp, 2, stringsAsFactors = FALSE)
+readGoogleDocs(gid = 2, key = key)
+
+readGoogleDocs <- function(gid = NA, key = NA) {
+  # code from R bloggers
+  # http://www.r-bloggers.com/r-function-to-read-data-from-google-docs-spreadsheets/
+  if (is.na(gid)) {
+    stop('\nWorksheetnumber (gid) is missing\n')
+    }
+  if (is.na(key)) {
+    stop('\nDocumentkey (key) is missing\n')
+    }
+  require(RCurl)
+  url <- getURL(paste('https://docs.google.com/spreadsheet/pub?key=', key,
+                      '&single=true&gid=', gid, '&output=csv', sep = ''),
+                cainfo = system.file('CurlSSL', 'cacert.pem', package = 'RCurl'))
+  return(read.xlsx(textConnection(url), header = T, sep = ','))
+}
