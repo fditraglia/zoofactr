687b9ee39a91f072b8e5d9e4e35723dac35659c0
Francis DiTraglia <fditra@sas.upenn.edu>
2016-01-24 14:51:21 -0500

    update class constructor for t model

--- src/sampler_t.cpp
+++ src/sampler_t.cpp
@@ -7,20 +7,20 @@ using namespace Rcpp;
 class SURt {
   public:
     SURt(const arma::mat&, const arma::mat&, const arma::mat&,
-                 const arma::vec&, const arma::mat&, int, int, int);
+                 const arma::vec&, const arma::mat&, int, int, int, int);
     double logML();
-    arma::mat g_draws, Omega_inv_draws;
-//  private:
-    int r1, T, D, K, p, n_vech, j, r0copy;
-    arma::vec G0_inv_g0, gbar, g, g0copy;
-    arma::mat XX, XY, R0_inv, G0_inv;
+    arma::mat g_draws, Omega_inv_draws, lambda_draws;
+//  private: //uncomment later after everything has been thoroughly tested
+    int r1, a1, T, D, K, p, n_vech, j, r0copy, nucopy;
+    arma::vec G0_inv_g0, gbar_lambda, g, g0copy, lambda;
+    arma::mat R0_inv, G0_inv;
     arma::mat Xcopy, Ycopy, G0copy, R0copy;
-    arma::mat resid, RT, GT_inv, Omega_inv, RT_draws;
+    arma::mat resid, R_Tlambda, G_Tlambda_inv, Omega_inv, RT_draws;
 };
 //Class constructor
 SURt::SURt(const arma::mat& X, const arma::mat& Y,
                            const arma::mat& G0, const arma::vec& g0,
-                           const arma::mat& R0, int r0,
+                           const arma::mat& R0, int r0, int nu,
                            int n_draws = 1000,
                            int burn_in = 1000){
   T = Y.n_rows;
@@ -41,27 +41,34 @@ SURt::SURt(const arma::mat& X, const arma::mat& Y,
   Xcopy = X;
   Ycopy = Y;
 
-  XX = X.t() * X;
-  XY = X.t() * Y;
   r1 = r0 + T;
+  a1 = 0.5 * (nu + D); // Gamma shape parameter for lambda draws
   R0_inv = inv_sympd(R0);
   G0_inv = inv_sympd(G0);
   G0_inv_g0 = solve(symmatu(G0), g0);
 
+  //Starting values
   Omega_inv = arma::eye(D, D);
+  lambda = arma::ones(T);
 
   for(int i = 0; i < (n_draws + burn_in); i++){
-
-    GT_inv = G0_inv + kron(Omega_inv, XX);
-    gbar = solve(GT_inv, G0_inv_g0 + vectorise(XY * Omega_inv));
-    g = draw_normal(gbar, GT_inv);
+    //draw gamma
+    G_Tlambda_inv = G0_inv + kron(Omega_inv, X.t() * arma::diagmat(lambda) * X);
+    gbar_lambda = solve(G_Tlambda_inv, G0_inv_g0 +
+      vectorise(X.t() * arma::diagmat(lambda) * Y * Omega_inv));
+    g = draw_normal(gbar_lambda, G_Tlambda_inv);
+    //construct matrix of errors given gamma - each row is a time period
     resid = Y - X * reshape(g, K, D);
-    RT = inv_sympd(R0_inv + resid.t() * resid);
-    Omega_inv = draw_wishart(r1, RT);
+    //draw Omega_inv
+    R_Tlambda = inv_sympd(R0_inv + resid.t() * arma::diagmat(lambda) * resid);
+    Omega_inv = draw_wishart(r1, R_Tlambda);
+    //draw lambda
+    lambda = draw_gamma(a1, 0.5 * (nu + arma::sum(arma::pow(resid, 2), 2)));
 
     if(i >= burn_in){
       j = i - burn_in;
-      RT_draws.col(j) = vech(RT);
+      //Think carefully about what I need to store for ML calculation...
+      RT_draws.col(j) = vech(R_Tlambda);
       Omega_inv_draws.col(j) = vech(Omega_inv);
       g_draws.col(j) = g;
     }
@@ -101,9 +108,9 @@ double SURt::logML(){
 
 // [[Rcpp::export]]
 List samplerTest(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
-                 arma::mat R0, int r0,
+                 arma::mat R0, int r0, int nu,
                  int n_draws, int burn_in){
-  SURt draws(X, Y, G0, g0, R0, r0, n_draws, burn_in);
+  SURt draws(X, Y, G0, g0, R0, r0, nu, n_draws, burn_in);
   List out = List::create(Named("g_draws") = draws.g_draws,
           Named("Omega_inv_draws") = draws.Omega_inv_draws,
           Named("logML") = draws.logML());
@@ -112,9 +119,9 @@ List samplerTest(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
 
 // [[Rcpp::export]]
 double logML_SUR(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
-                 arma::mat R0, int r0,
+                 arma::mat R0, int r0, int nu,
                  int n_draws = 5000, int burn_in = 1000){
-  SURt draws(X, Y, G0, g0, R0, r0, n_draws, burn_in);
+  SURt draws(X, Y, G0, g0, R0, r0, nu, n_draws, burn_in);
   return draws.logML();
 }
 
@@ -128,34 +135,10 @@ List defaultSUR(arma::mat X, arma::mat Y, double coef_scale = 10,
   arma::vec g0 = arma::zeros(p);
   arma::mat G0 = pow(coef_scale, 2) * arma::eye(p, p);
   int r0 = d + 2;
+  int nu = 5;
   arma::mat R0 = arma::eye(d, d) / (pow(cov_scale, 2) * r0);
-  SURt draws(X, Y, G0, g0, R0, r0, 5000, 1000);
+  SURt draws(X, Y, G0, g0, R0, r0, nu, 5000, 1000);
   List out = List::create(Named("g_draws") = draws.g_draws,
           Named("Omega_inv_draws") = draws.Omega_inv_draws);
   return out;
 }
-
-/*** R
-# setwd("~/factor-choice/")
-# dat <- read.csv("data_value.csv")
-# nT <- nrow(dat)
-# x1 <- rep(1, nT)
-# x2 <- dat$Mkt.RF
-# x3 <- dat$SMB
-# x4 <- dat$HML
-# M <- matrix(c(1, 0.2, 0.2, 1), 2, 2)
-# set.seed(1234)
-# errors <- t(chol(M) %*% rbind(rnorm(nT), rnorm(nT)))
-# y1 <- x2 + errors[,1]
-# y2 <- 0.5 + x2 + 0.2 * x3 + 0.2 * x4 + errors[,2]
-# Y <- cbind(y1, y2)
-# X <- cbind(x1, x2, x3, x4)
-# rm(x1, x2, x3, x4, y1, y2, nT, dat)
-# r0 <- 10
-# g0 <- rep(0, ncol(X) * ncol(Y))
-# G0 <- diag(ncol(X) * ncol(Y))
-# R0 <- diag(ncol(Y))
-# gibbs <- samplerTest(X, Y, G0, g0, R0, r0, 4000, 1000)
-# solve(devech(rowMeans(gibbs$Omega_inv_draws), 2))
-# matrix(rowMeans(gibbs$g_draws), ncol(X), ncol(Y))
-*/
