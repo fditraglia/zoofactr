036bda7edd702f69c12c9dc5b6e7711310d7dcf5
Francis DiTraglia <fditra@sas.upenn.edu>
2016-01-25 15:49:22 -0500

    more work on t sampler: name private class variables to match the arguments of which they are copies using an initialization list

--- src/sampler_t.cpp
+++ src/sampler_t.cpp
@@ -10,19 +10,24 @@ class SURt {
                  const arma::vec&, const arma::mat&, int, int, int, int);
     double logML();
     arma::mat g_draws, Omega_inv_draws, lambda_draws;
-//  private: //uncomment later after everything has been thoroughly tested
-    int r1, a1, T, D, K, p, n_vech, j, r0copy, nucopy;
-    arma::vec G0_inv_g0, gbar_lambda, g, g0copy, lambda;
+  private:
+    int r1, a1, T, D, K, p, n_vech, j;
+    arma::vec G0_inv_g0, gbar_lambda, g, lambda;
     arma::mat R0_inv, G0_inv;
-    arma::mat Xcopy, Ycopy, G0copy, R0copy;
     arma::mat resid, R_Tlambda, G_Tlambda_inv, Omega_inv, R_Tlambda_draws;
+    //Private copies of prior and other input parameters with same names
+    //as the corresponding function arguments using an initialization list
+    const int r0, nu, n_draws, burn_in;
+    const arma::mat X, Y, G0, R0;
+    const arma::vec g0;
 };
 //Class constructor
-SURt::SURt(const arma::mat& X, const arma::mat& Y,
-                           const arma::mat& G0, const arma::vec& g0,
-                           const arma::mat& R0, int r0, int nu,
-                           int n_draws = 1000,
-                           int burn_in = 1000){
+SURt::SURt(const arma::mat& X, const arma::mat& Y, const arma::mat& G0,
+           const arma::vec& g0,const arma::mat& R0, int r0, int nu,
+           int n_draws = 1000, int burn_in = 1000): r0(r0), nu(nu),
+           n_draws(n_draws), burn_in(burn_in), X(X), Y(Y), G0(G0), R0(R0),
+           g0(g0){
+
   T = Y.n_rows;
   D = Y.n_cols;
   K = X.n_cols;
@@ -31,15 +36,7 @@ SURt::SURt(const arma::mat& X, const arma::mat& Y,
 
   Omega_inv_draws.zeros(n_vech, n_draws);
   g_draws.zeros(p, n_draws);
-  RT_draws.zeros(n_vech, n_draws);
-
-  r0copy = r0;
-  G0copy = G0;
-  g0copy = g0;
-  r0copy = r0;
-  R0copy = R0;
-  Xcopy = X;
-  Ycopy = Y;
+  R_Tlambda_draws.zeros(n_vech, n_draws);
 
   r1 = r0 + T;
   a1 = 0.5 * (nu + D); // Gamma shape parameter for lambda draws
@@ -73,6 +70,7 @@ SURt::SURt(const arma::mat& X, const arma::mat& Y,
     }
   }
 }
+
 //Member function to calculate marginal likelihood
 double SURt::logML(){
   //Posterior means for gamma and Omega inverse
@@ -80,29 +78,27 @@ double SURt::logML(){
   arma::mat Omega_inv_star = devech(mean(Omega_inv_draws, 1), D);
 
   //Contribution of prior - identical to model with normal likelihood
-  double prior1 = as_scalar(density_normal(gstar, g0copy,
-                                           G0copy, true));
-  double prior2 = density_wishart(Omega_inv_star, r0copy,
-                                  R0copy, true);
+  double prior1 = as_scalar(density_normal(gstar, g0, G0, true));
+  double prior2 = density_wishart(Omega_inv_star, r0, R0, true);
 
   //Residuals evaluated at posterior mean of gamma: each row is a time period
-  arma::mat resid_star =  Ycopy - Xcopy * reshape(gstar, K, D);
+  arma::mat resid_star =  Y- X* reshape(gstar, K, D);
 
   //Contribution of likelihood: transpose residuals since density_t expects
   //                            each column to be one observation (time period)
-  double like = sum(density_t(resid_star.t(), nucopy, arma::zeros<arma::vec>(D),
+  double like = sum(density_t(resid_star.t(), nu, arma::zeros<arma::vec>(D),
                                    Omega_inv_star, true));
 
   //First Term of Posterior Contribution: Omega inverse
-  arma::vec post1_terms(R_Tlambda_draws.n_cols);
+  arma::vec post1_terms(n_draws);
   arma::mat R_Tlambda_g(D, D);
-  for(int i = 0; i < R_Tlambda_draws.n_cols; i++){
+  for(int i = 0; i < n_draws; i++){
     R_Tlambda_g = devech(R_Tlambda_draws.col(i), D);
     post1_terms(i) = density_wishart(Omega_inv_star, r1, R_Tlambda_g);
   }
   double post1 = log(mean(post1_terms));
 
-  //Second term of Posterior Constribution: gamma (Reduced run!)
+  //Second term of Posterior Constribution: gamma
   arma::vec post2_terms;
   // Implement with same number of draws and burn-in as original samples
   //      In the reduced run hold Omega inverse fixed at prior mean
@@ -116,7 +112,7 @@ double SURt::logML(){
 
 
 // [[Rcpp::export]]
-List samplerTest(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
+List samplerTest_t(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
                  arma::mat R0, int r0, int nu,
                  int n_draws, int burn_in){
   SURt draws(X, Y, G0, g0, R0, r0, nu, n_draws, burn_in);
@@ -127,7 +123,7 @@ List samplerTest(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
 }
 
 // [[Rcpp::export]]
-double logML_SUR(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
+double logML_SUR_t(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
                  arma::mat R0, int r0, int nu,
                  int n_draws = 5000, int burn_in = 1000){
   SURt draws(X, Y, G0, g0, R0, r0, nu, n_draws, burn_in);
@@ -135,7 +131,7 @@ double logML_SUR(arma::mat X, arma::mat Y, arma::mat G0, arma::vec g0,
 }
 
 // [[Rcpp::export]]
-List defaultSUR(arma::mat X, arma::mat Y, double coef_scale = 10,
+List defaultSUR_t(arma::mat X, arma::mat Y, double coef_scale = 10,
               double cov_scale = 10){
 // If X ~ Wishart_d(v, S) then E[X] = v * S
 // and E[X^{-1}] = S^{-1} / (v - d - 1)
